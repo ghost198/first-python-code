1f89c326454615d5e466a7322a53ad733bf3ea2f
Faisal Amar <faisalamar94@gmail.com>
2025-02-16 22:29:15 +0700

    input nama

---
+++ tes.py
@@ -0,0 +1,9 @@
+
+
+nama = input("masukkan nama kamu ")
+umur = input("berapa umur kamu? ")
+print(f"wiiih salam kenal yaaa {nama} di umur ke {umur} ini apa planning mu ?")
+input()
+
+if (input):
+    print("semoga tercapai ya")

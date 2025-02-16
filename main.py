0c528ea0c623faf68aaf3d6abe3994ee298ae060
Faisal Amar <faisalamar94@gmail.com>
2025-02-16 20:46:04 +0700

    looping

---
+++ main.py
@@ -0,0 +1,12 @@
+from selectors import SelectSelector
+
+jumlah_buku = 100
+print('ibu berkata', "Baca Bukumu")
+
+jumlah_buku_yang_belumm_dibaca = 0
+for jumlah_buku_yang_belum_dibaca in range(1, jumlah_buku+1):
+    print(f"baca buku {jumlah_buku_yang_belum_dibaca}")
+else:
+    print("semua buku telah dibaca")
+
+

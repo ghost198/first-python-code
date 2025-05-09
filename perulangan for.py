2ae2c17c76e5fce4f5664207260ac1796627e6c1
Faisal Amar <faisalamar94@gmail.com>
2025-05-09 21:18:37 +0700

    menambahkan halo

--- perulangan for.py
+++ perulangan for.py
@@ -5,6 +5,7 @@ print('perintah ibu, "Baca semua bukumu"')
 jumlah_buku_yang_dibaca = 0
 print(f'baik bu, "saat ini buku yang belum saya baca" {jumlah_buku_yang_dibaca}')
 print("saya akan mulai membaca")
+print("test, halo")
 
 for jumlah_buku_yang_dibaca in range(1, jumlah_buku+1):
     print(f"buku ke {jumlah_buku_yang_dibaca} sudah dibaca ")
